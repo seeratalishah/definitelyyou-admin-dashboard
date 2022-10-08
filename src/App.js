1653c4f7e287cabcad5c12cf9047a515a3193716
Tahira-Malik <tahiramalik6644@gmail.com>
2022-10-08 21:53:36 +0500

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import DashboardLayout from "./layout/DashboardLayout";
+import OrdersPage from "./pages/OrdersPage";
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<DashboardLayout />}></Route>
+      <Route path="/dashboard" element={<DashboardLayout />}>
+        <Route path="orders" element={<OrdersPage/>}></Route>
+      </Route>
     </Routes>
   );
 }
---
+++ src/pages/OrdersPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OrdersPage() {
+  return (
+    <h1>tahira</h1>
+  )
+}
+
+export default OrdersPage

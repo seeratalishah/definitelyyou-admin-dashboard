cd3841f2f29ccccf792515d71bd2e66070e0de15
Tahira-Malik <tahiramalik6644@gmail.com>
2022-10-08 23:55:15 +0500

    remove unnecessary components

--- src/pages/OrdersPage.js
+++ src/pages/OrdersPage.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import DropDownMenu from 'material-ui/DropDownMenu';
 
 function OrdersPage() {
   return (
     <div>
       <h2>All Orders</h2>
-      <DropDownMenu 
-          value={this.state.selection} 
-          onChange={this.handleChange}   
-         >
-          <MenuItem value={1} primaryText="English"  />
-          <MenuItem value={2} primaryText="Spanish" />
-          <MenuItem value={3} primaryText="French" />
-
-        </DropDownMenu>
+    
     </div>
     
     

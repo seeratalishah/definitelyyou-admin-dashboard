197e5ac71161e514b77e2b4ba0374c097c8e3411
seeratalishah <seeratalyshah@gmail.com>
2022-10-09 12:43:34 +0500

    edit product page

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -3,85 +3,70 @@ import React from 'react'
 
 function ProductsPage() {
   return (
-    <div className='products-page'>
-        <div className="add-prodcut">
-            <h1>All Products</h1>
-            <button>Add Product</button>
-            <a href="">hello</a>
-        </div>
-    </div>
+    <div>
+    <h2>All Products</h2>
+    <div className="table">
+    <tr>
+      <th>All</th>
+      <th>Product Name </th>
+      <th>Price</th>
+      <th>Inventery</th>
+      <th>Category</th>
+      <th>Date</th>
+      <th>Customer Id</th>
+    </tr>
+    <tr>
+      <td>img1</td>
+      <td>Frock</td>
+      <td>1000pkr</td>
+      <td>4 in stock</td>
+      <td>Fancy</td>
+      <td>02-08-22</td>
+      <td>01</td>
+    </tr>
+    <tr>
+      <td>img2</td>
+      <td>Frock</td>
+      <td>2500pkr</td>
+      <td>1 in stock</td>
+      <td>Fancy</td>
+      <td>12-08-22</td>
+      <td>05</td>
+    </tr>
+    <tr>
+      <td>img3</td>
+      <td>Frock</td>
+      <td>2000pkr</td>
+      <td>1 in stock</td>
+      <td>Fancy</td>
+      <td>02-08-22</td>
+      <td>01</td>
+    </tr>
+    <tr>
+      <td>img4</td>
+      <td>Frock</td>
+      <td>3000pkr</td>
+      <td>7 in stock</td>
+      <td>Fancy</td>
+      <td>06-08-22</td>
+      <td>09</td>
+    </tr>
+    <tr>
+      <td>img5</td>
+      <td>Frock</td>
+      <td>1000pkr</td>
+      <td>4 in stock</td>
+      <td>Fancy</td>
+      <td>02-08-22</td>
+      <td>01</td>
+    </tr>
+  </div>
+  </div>
+ 
   )
 }
 
 export default ProductsPage
 
-import React from 'react';
 
-function OrdersPage() {
-  return (
-    <div>
-      <h2>All Products</h2>
-      <div className="table">
-      <tr>
-        <th>All</th>
-        <th>Product Name </th>
-        <th>Price</th>
-        <th>Inventery</th>
-        <th>Category</th>
-        <th>Date</th>
-        <th>Customer Id</th>
-      </tr>
-      <tr>
-        <td>img1</td>
-        <td>Frock</td>
-        <td>1000pkr</td>
-        <td>4 in stock</td>
-        <td>Fancy</td>
-        <td>02-08-22</td>
-        <td>01</td>
-      </tr>
-      <tr>
-        <td>img2</td>
-        <td>Frock</td>
-        <td>2500pkr</td>
-        <td>1 in stock</td>
-        <td>Fancy</td>
-        <td>12-08-22</td>
-        <td>05</td>
-      </tr>
-      <tr>
-        <td>img3</td>
-        <td>Frock</td>
-        <td>2000pkr</td>
-        <td>1 in stock</td>
-        <td>Fancy</td>
-        <td>02-08-22</td>
-        <td>01</td>
-      </tr>
-      <tr>
-        <td>img4</td>
-        <td>Frock</td>
-        <td>3000pkr</td>
-        <td>7 in stock</td>
-        <td>Fancy</td>
-        <td>06-08-22</td>
-        <td>09</td>
-      </tr>
-      <tr>
-        <td>img5</td>
-        <td>Frock</td>
-        <td>1000pkr</td>
-        <td>4 in stock</td>
-        <td>Fancy</td>
-        <td>02-08-22</td>
-        <td>01</td>
-      </tr>
-    </div>
-    </div>
-   
-    
-  );
-}
-
-export default OrdersPage;
 

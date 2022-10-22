8b6f4893d186b88fbc9163353346f0519dd4243f
seeratalishah <seeratalyshah@gmail.com>
2022-10-22 13:10:30 +0500

    Added Dashboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import DashboardLayout from "./layout/DashboardLayout";
 import AddProductPage from "./pages/AddProductPage";
+import CustomersPage from "./pages/CustomersPage";
+import DashboardPage from "./pages/DashboardPage";
 import OrdersPage from "./pages/OrdersPage";
 import ProductsPage from './pages/ProductsPage';
 
@@ -11,9 +13,11 @@ function App() {
     <Routes>
       <Route path="/" element={<DashboardLayout />}>
 
+        <Route index element={<DashboardPage />} />
         <Route path="products" element={<ProductsPage/>}></Route>
         <Route path="orders" element={<OrdersPage/>}></Route>
         <Route path="addproduct" element={<AddProductPage />}></Route>
+        <Route path="customers" element={<CustomersPage />} />
 
       </Route>
     </Routes>

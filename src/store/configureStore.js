b4b48e98cc0867e35c514d637f216a3e611a4b8b
Tahira-Malik <85984818+Tahiraawan@users.noreply.github.com>
2022-10-29 14:05:14 +0500

    delete

--- src/store/configureStore.js
+++
@@ -1,8 +0,0 @@
-import userSliceReducer from './userSlice'
-import { configureStore } from "@reduxjs/toolkit";
-//create store..
-export const store=configureStore({
-reducer:{
-user: userSliceReducer
-}
-});
